cc57118431ad7c76d84425915e11905a42490cfd
Timmy tam Luu <tluu@fu-r9-p1.s19.be>
2022-09-27 15:02:27 +0200

    trollollo

--- script.js
+++ script.js
@@ -1,38 +1,70 @@
-const contentText = document.querySelector("#content-text");
-const inputField = document.querySelector("#invisible-text");
-let charIndex = 0;
+const   contentText = document.querySelector("#content-text");
+const   inputField = document.querySelector("#invisible-text");
+const   timerText = document.querySelector("#timer");
+const   wpmText = document.querySelector("#wpm");
+const   accText = document.querySelector("#acc");
 
+let timerInterval;
+let inGame = false;
+
+let maxTime = 30;
+let timeLeft = maxTime;
+
+let mistake = 0;
 
 const paragraph = [
     "trol loll ol",
-    "je suis un chololqt",
+    "je suis un chololat",
     "je suis un fromage",
     "chocolat blanc"
 ];
 
-function randomParagraph() {
+function    randomParagraph() {
     let randomSentence = Math.floor(Math.random() * paragraph.length);
-    paragraph[randomSentence].split("").forEach(span => {
-        let spanTag = `<span>${span}</span>`;
-        contentText.innerHTML += spanTag;
+    paragraph[randomSentence].split('').forEach(span => {
+        const characterSpan = document.createElement('span')
+        characterSpan.innerText = span;
+        contentText.appendChild(characterSpan);
     });
     document.addEventListener("keydown", () => inputField.focus());
     contentText.addEventListener("click", () => inputField.focus());
 }
 
-function initTyping() {
-    const characters = contentText.querySelectorAll("span").characters;
-    let typedChar = inputField.value.split("")[charIndex];
-    console.log(characters[charIndex]);
-    console.log(typedChar); 
-    if (characters[charIndex] == typedChar) {
-        console.log("correct");
+function    gameInit() {
+    const characters = contentText.querySelectorAll('span');
+    const typedChar = inputField.value.split('');
+
+    if (!inGame) {
+        inGame = true;
+        timerInterval = setInterval(myTimer, 1000);
     }
-    else {
-        console.log("incorrect");
+    typedChar.forEach((charac, index) => {
+        if (charac === characters[index].firstChild.nodeValue)
+        {
+            characters[index].classList.add("correct");
+            characters[index].classList.remove("fail");
+            console.log("succes");
+        }
+        else
+        {
+            characters[index].classList.add("fail");
+            characters[index].classList.remove("correct");
+            mistake++;
+            console.log("false");
+        }
+    });
+}
+
+function    myTimer() {
+    var timer;
+
+    if (timeLeft > 0) {
+        timeLeft--;
+        timerText.innerHTML = timeLeft;
     }
-    charIndex++;
+    else
+        clearInterval(timerInterval);
 }
 
 randomParagraph();
-inputField.addEventListener("input", initTyping);
+inputField.addEventListener('input', gameInit);
