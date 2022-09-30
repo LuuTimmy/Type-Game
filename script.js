25d2f62e341c12377f439e10297b8f676c5286ac
Timmy tam Luu <tluu@fu-r3-p8.s19.be>
2022-09-30 20:29:20 +0200

    eopjpogegwe

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const   contentText = document.querySelector("#content-text");
-const   inputField = document.querySelector("#invisible-text");
-const   timerText = document.querySelector("#timer");
-const   wpmText = document.querySelector("#wpm");
-const   accText = document.querySelector("#acc");
+const contentText = document.querySelector("#content-text");
+const inputField = document.querySelector("#invisible-text");
+const timerText = document.querySelector("#timer");
+const wpmText = document.querySelector("#wpm");
+const accText = document.querySelector("#acc");
+const tryAgainButton = document.querySelector("#try-again");
+const newTextButton = document.querySelector("#new-text");
 
 let nbtype;
 let lengthPhrase;
@@ -14,85 +16,102 @@ let timeLeft;
 let mistake;
 
 const paragraph = [
-    "trol loll ol",
-    "je suis un chololat",
-    "je suis un fromage",
-    "chocolat blanc"
+  "trol loll ol",
+  "je suis un chololat",
+  "je suis un fromage",
+  "chocolat blanc",
 ];
 
-function    initarg() {
-    nbtype = 0;
-    charIndex = 0;
-    inGame = false;
-    maxTime = 30;
-    timeLeft = maxTime;
-    mistake = 0;
+function initarg() {
+  nbtype = 0;
+  charIndex = 0;
+  inGame = false;
+  maxTime = 30;
+  timeLeft = maxTime;
+  mistake = 0;
+
+  timerText.innerHTML = maxTime;
+  wpmText.innerHTML = 0;
+  accText.innerHTML = 0;
 }
 
-function    randomParagraph() {
-    let randomSentence = Math.floor(Math.random() * paragraph.length);
-    paragraph[randomSentence].split('').forEach(span => {
-        const characterSpan = document.createElement('span')
-        characterSpan.innerText = span;
-        contentText.appendChild(characterSpan);
+function randomParagraph() {
+  let randomSentence = Math.floor(Math.random() * paragraph.length);
+  paragraph[randomSentence].split("").forEach((span) => {
+    const characterSpan = document.createElement("span");
+    characterSpan.innerText = span;
+    contentText.appendChild(characterSpan);
+  });
+  document.addEventListener("keydown", () => inputField.focus());
+//   contentText.addEventListener("click", () => inputField.focus());
+}
+
+function game(event) {
+  const characters = contentText.querySelectorAll("span");
+  lengthPhrase = characters.length;
+  const typedChar = inputField.value.split("")[charIndex];
+  if (!inGame) {
+    inGame = true;
+    timerInterval = setInterval(myTimer, 1000);
+  }
+  if (event.inputType === "insertText") {
+    if (characters[charIndex].firstChild.nodeValue == typedChar) {
+      characters[charIndex].className = "correct";
+      console.log("correct");
+    } else {
+      mistake++;
+      characters[charIndex].className = "fail";
+      console.log("failure");
+    }
+    nbtype++;
+    if (charIndex != lengthPhrase - 1) charIndex++;
+    else {
+      inGame = false;
+      alert("youwin");
+    }
+  }
+  const nbCorrect = contentText.querySelectorAll(".correct").length;
+  wpmText.innerText = Math.round(nbCorrect / 5) * (60 / maxTime);
+  accText.innerText = Math.round(100 - (mistake / nbtype) * 100);
+}
+
+function myTimer() {
+  var timer;
+
+  if (timeLeft > 0 && inGame) {
+    timeLeft--;
+    timerText.innerText = timeLeft;
+  } 
+  else 
+    clearInterval(timerInterval);
+}
+
+function resetGame() {
+    contentText.querySelectorAll("span").forEach((arg) => {
+        arg.className = '';
     });
-    document.addEventListener("keydown", () => inputField.focus());
-    contentText.addEventListener("click", () => inputField.focus());
+    clearInterval(timerInterval);
+    initarg();
 }
 
-function    game(event) {
-    const characters = contentText.querySelectorAll('span');
-    lengthPhrase = characters.length;
-    const typedChar = inputField.value.split('')[charIndex];
-    if (!inGame) {
-        inGame = true;
-        timerInterval = setInterval(myTimer, 1000);
-    }
-    if (event.inputType === "insertText") {
-        if (characters[charIndex].firstChild.nodeValue == typedChar) {
-            characters[charIndex].classList.remove("fail");
-            characters[charIndex].classList.add("correct");
-            console.log("succes");
-        }
-        else {
-            mistake++;
-            characters[charIndex].classList.remove("correct");
-            characters[charIndex].classList.add("fail");
-            console.log("failure");
-        }
-        nbtype++;
-        if (charIndex != lengthPhrase - 1)
-            charIndex++;
-        else {
-            inGame = false;
-            alert("youwin");
-        }
-    }
-    const nbCorrect = contentText.querySelectorAll('.correct').length;
-    wpmText.innerText = Math.round((nbCorrect / 5)) * (60 / maxTime);
-    accText.innerText = Math.round(100 - (mistake / nbtype * 100));
+function newText() {
+    resetGame();
+    contentText.innerHTML = '';
+    randomParagraph();
 }
-
-function    myTimer() {
-    var timer;
-
-    if (timeLeft > 0 && inGame) {
-        timeLeft--;
-        timerText.innerText = timeLeft;
-    }
-    else
-        clearInterval(timerInterval);
-}
-
 
 initarg();
 randomParagraph();
-inputField.addEventListener('input', game);
-inputField.addEventListener('keydown', (event) => {
-    if (charIndex > 0 && (event.key === "Backspace" || event.key === "Delete"))
-        charIndex--;
-    if (charIndex > 0 && event.key === "ArrowLeft")
-        charIndex--;
-    if (charIndex < lengthPhrase - 1 && event.key === "RightArrow")
-        charIndex++;
+inputField.addEventListener("input", game);
+tryAgainButton.addEventListener("click", resetGame);
+newTextButton.addEventListener("click", newText);
+inputField.addEventListener("keydown", (event) => {
+  if (charIndex > 0 && (event.key === "Backspace" || event.key === "Delete")) {
+    charIndex--;
+    contentText.querySelectorAll("span")[charIndex].className = '';
+  }
+  if (charIndex > 0 && event.key === "ArrowLeft") 
+    charIndex--;
+  if (charIndex < lengthPhrase - 1 && event.key === "RightArrow") 
+    charIndex++;
 });
