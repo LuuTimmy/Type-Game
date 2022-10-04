e045aa57b8f800a42c879f0f2fa3b04d1cdb17b4
Luu Timmy <timmyluu00@gmail.com>
2022-10-05 01:20:04 +0200

    yo

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const accText = document.querySelector("#acc");
 const tryAgainButton = document.querySelector("#try-again");
 const newTextButton = document.querySelector("#new-text");
 
-const random_phrase = "http://api.quotable.io/random?minLength=200&maxLength=500";
+const random_quote = "http://api.quotable.io/random?minLength=200&maxLength=500";
 
 let nbtype;
 let lengthPhrase;
@@ -17,120 +17,124 @@ let maxTime;
 let timeLeft;
 let mistake;
 
-function getRandomQuote() {
-  return fetch(random_phrase)
-    .then((response) => response.json())
-    .then((data) => data.content);
-}
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e094cca14amsh94fe50f8bed3647p1fab90jsn17edbeb6e063',
+		'X-RapidAPI-Host': 'random-words5.p.rapidapi.com'
+	}
+};
 
-const paragraph = [
-  "trol loll ol",
-  "je suis un chololat",
-  "je suis un fromage",
-  "chocolat blanc",
-];
+function getRandomQuote() {
+	return fetch('https://random-words5.p.rapidapi.com/getMultipleRandom?count=20&maxLength=10', options)
+		.then(response => response.json())
+		.then(response => response)
+		.catch(err => console.error(err));
+}
 
 async function getNextQuote() {
-  const quote = await getRandomQuote();
-  quote.split("").forEach((span) => {
-    const characterSpan = document.createElement("span");
-    characterSpan.innerText = span;
-    contentText.appendChild(characterSpan);
-  });
-  document.addEventListener("keydown", () => inputField.focus());
-  contentText.addEventListener("click", () => inputField.focus());
-  console.log(quote);
+	const quote = await getRandomQuote();
+	quote.forEach((arg) => {
+		var characterSpan;
+		for(var i = 0; i <= arg.length; i++) {
+			characterSpan = document.createElement("span");
+			characterSpan.innerText = arg[i];
+			contentText.appendChild(characterSpan);
+		}
+		characterSpan.innerText = " ";
+	});
+	console.log(quote);
 }
-
-getNextQuote();
 
 function initarg() {
-  nbtype = 0;
-  charIndex = 0;
-  inGame = false;
-  maxTime = 30;
-  timeLeft = maxTime;
-  mistake = 0;
+	nbtype = 0;
+	charIndex = 0;
+	inGame = false;
+	maxTime = document.querySelector('input[name="timer"]:checked').value;
+	timeLeft = maxTime;
+	mistake = 0;
 
-  timerText.innerHTML = maxTime;
-  wpmText.innerHTML = 0;
-  accText.innerHTML = 0;
-}
-
-function randomParagraph() {
-  let randomSentence = Math.floor(Math.random() * paragraph.length);
-  paragraph[randomSentence].split("").forEach((span) => {
-    const characterSpan = document.createElement("span");
-    characterSpan.innerText = span;
-    contentText.appendChild(characterSpan);
-  });
-  document.addEventListener("keydown", () => inputField.focus());
-  contentText.addEventListener("click", () => inputField.focus());
+	timerText.innerHTML = maxTime;
+	wpmText.innerHTML = 0;
+	accText.innerHTML = 0;
 }
 
 function game(event) {
-  const characters = contentText.querySelectorAll("span");
-  lengthPhrase = characters.length;
-  const typedChar = inputField.value.split("")[charIndex];
-  if (!inGame) {
-    inGame = true;
-    timerInterval = setInterval(myTimer, 1000);
-  }
-  if (event.inputType === "insertText") {
-    if (characters[charIndex].firstChild.nodeValue == typedChar) {
-      characters[charIndex].className = "correct";
-      console.log("correct");
-    } else {
-      mistake++;
-      characters[charIndex].className = "fail";
-      console.log("failure");
-    }
-    nbtype++;
-    if (charIndex != lengthPhrase - 1) charIndex++;
-    else {
-      inGame = false;
-      alert("youwin");
-    }
-  }
-  const nbCorrect = contentText.querySelectorAll(".correct").length;
-  wpmText.innerText = Math.round(nbCorrect / 5) * (60 / maxTime);
-  accText.innerText = Math.round(100 - (mistake / nbtype) * 100);
+	const characters = contentText.querySelectorAll("span");
+	lengthPhrase = characters.length;
+	const typedChar = inputField.value.split("")[charIndex];
+	if (!inGame) {
+		inGame = true;
+		timerInterval = setInterval(myTimer, 1000);
+	}
+	if (event.inputType === "insertText") {
+		if (characters[charIndex].firstChild.nodeValue == typedChar) {
+			characters[charIndex].className = "correct";
+			console.log("correct");
+		} else {
+			mistake++;
+			characters[charIndex].className = "fail";
+			console.log("failure");
+		}
+		nbtype++;
+		if (charIndex != lengthPhrase - 1) 
+			charIndex++;
+		else {
+			inGame = false;
+		alert("youwin");
+		}
+	}
+	const nbCorrect = contentText.querySelectorAll(".correct").length;
+	wpmText.innerText = Math.round((nbCorrect / 5) * (60 / maxTime));
+	accText.innerText = Math.round(100 - (mistake / nbtype) * 100);
 }
 
 function myTimer() {
-  var timer;
+	var timer;
 
-  if (timeLeft > 0 && inGame) {
-    timeLeft--;
-    timerText.innerText = timeLeft;
-  } else clearInterval(timerInterval);
+	if (timeLeft > 0 && inGame) {
+		timeLeft--;
+		timerText.innerText = timeLeft;
+	} else clearInterval(timerInterval);
 }
 
 function resetGame() {
-  contentText.querySelectorAll("span").forEach((arg) => {
-    arg.className = "";
-  });
-  clearInterval(timerInterval);
-  initarg();
+    contentText.querySelectorAll("span").forEach((arg) => {
+        arg.className = "";
+    });
+    clearInterval(timerInterval);
+    initarg();
 }
 
 function newText() {
   resetGame();
   contentText.innerHTML = "";
-  //randomParagraph();
   getNextQuote();
 }
 
-initarg();
-//randomParagraph();
+newText();
+document.addEventListener("keydown", () => inputField.focus());
+contentText.addEventListener("click", () => inputField.focus());
+
 inputField.addEventListener("input", game);
 tryAgainButton.addEventListener("click", resetGame);
 newTextButton.addEventListener("click", newText);
+
 inputField.addEventListener("keydown", (event) => {
-  if (charIndex > 0 && (event.key === "Backspace" || event.key === "Delete")) {
-    charIndex--;
-    contentText.querySelectorAll("span")[charIndex].className = "";
-  }
-  if (charIndex > 0 && event.key === "ArrowLeft") charIndex--;
-  if (charIndex < lengthPhrase - 1 && event.key === "RightArrow") charIndex++;
+	if (charIndex > 0 && (event.key === "Backspace" || event.key === "Delete")) {
+		charIndex--;
+		contentText.querySelectorAll("span")[charIndex].className = "";
+	}
+	if (charIndex > 0 && event.key === "ArrowLeft") charIndex--;
+	if (charIndex < lengthPhrase - 1 && event.key === "RightArrow") charIndex++;
+});
+
+document.querySelectorAll('input[name="timer"]').forEach((arg) => {
+	arg.addEventListener("click", (args) => {
+		if (inGame == false) {
+			maxTime = document.querySelector('input[name="timer"]:checked').value;
+			timeLeft = maxTime;
+			timerText.innerText = maxTime;
+		}
+	});
 });
